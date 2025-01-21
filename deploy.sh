9796f3d078e6cb7ba10341facebf81f9bd377c84
yulilong <yulilong222q@163.com>
2025-01-21 10:05:40 +0800

    部署脚本命令优化：提示文字输出优化、关闭代码提交到码云

--- deploy.sh
+++ deploy.sh
@@ -1,52 +1,46 @@
 #!/usr/bin/env sh
 
-echo '开始执行命令'
-# 生成静态文件
-echo ''
+echo '开始执行命令：'
+
+echo '1、生成静态文件'
 echo '执行命令：gitbook build .'
 echo ''
 gitbook build .
 
-# 进入生成的文件夹
-echo ''
+echo '2、进入生成的文件夹'
 echo "执行命令：cd ./_book"
-echo ''
 cd ./_book
 
 # 发布到自定义域名
-echo ''
-echo "把域名放到CNAME文件中"
+echo '3、发布到自定义域名'
+echo '创建 CNAME 文件，并写入 yu66.vip值'
 echo 'yu66.vip' > CNAME
 echo ''
 
-# 初始化一个仓库，仅仅是做了一个初始化的操作，项目里的文件还没有被跟踪
-echo ''
+echo '4、初始化一个仓库'
 echo "执行命令：git init"
-echo ''
 git init
 
-# 保存所有的修改
-echo ''
+echo '5、添加所有文件到git'
 echo "执行命令：git add -A"
 echo ''
 git add -A
 
-# 把修改的文件提交
-echo ''
+echo '6、提交文件'
 echo "执行命令：commit -m 'deploy'"
 echo ''
 git commit -m 'deploy'
 
-## 新建一个分支，解决git init命令生成的默认分支名字不确定问题(旧版本是master，新版本(2.39.3)是main)
-echo ''
+# git init命令生成的默认分支名字：旧版本是master，新版本(2.39.3)是main
+echo '7、新建一个 front-end 分支，解决git init命令生成的默认分支名字不确定问题'
 echo "执行命令：git checkout -b front-end"
 git checkout -b front-end
 
 # 发布到码云网站
-echo ''
-echo "执行命令：git push -f https://gitee.com/dragon-li/my-wiki front-end:gh-pages"
-echo ''
-git push -f https://gitee.com/dragon-li/my-wiki front-end:gh-pages
+# echo '资源文件提交到 码云，用于 Gitee Pages 服务'
+# echo "执行命令：git push -f https://gitee.com/dragon-li/my-wiki front-end:gh-pages"
+# echo ''
+# git push -f https://gitee.com/dragon-li/my-wiki front-end:gh-pages
 
 # echo ""
 # echo "打开码云 Gitee Pages 服务，手动更新服务"
@@ -54,12 +48,11 @@ git push -f https://gitee.com/dragon-li/my-wiki front-end:gh-pages
 
 
 # 发布到github网站: https://<USERNAME>.github.io/<REPO>
-echo ''
-echo "发布到github网站服务，执行命令：git push -f git@github.com:yulilong/my_blog.git front-end:gh-pages"
+echo '资源文件提交到github上，用于GitHub Pages 服务'
+echo "执行命令：git push -f git@github.com:yulilong/my_blog.git front-end:gh-pages"
 git push -f git@github.com:yulilong/my_blog.git front-end:gh-pages
 
 
-# 返回到上一次的工作目录
-echo ''
-echo "回到刚才工作目录"
+echo '回到文档工作目录'
 cd -
+echo '前端代码打包发布结束'
